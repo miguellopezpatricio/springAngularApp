d30a68622311810e58468b8a2f000429496125ae
miguellopezpatricio <ml.patri@gmail.com>
2022-12-23 10:01:32 +0100

    Iniciando proyecto Angular con Spring

---
+++ angular/clientes-app/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title: string = 'Bienvenido a Angular'
+  curso: string = 'Spring 5 con Angular'
+
+
+
+}
